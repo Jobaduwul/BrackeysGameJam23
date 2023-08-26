9243b130abb8dccd09e0a116ff3ab08370f8f5cb
Kazi Jawadul Islam Jishan <kazijawadulislam123@gmail.com>
2023-08-26 15:08:56 +0600

    sensor added and moves up

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] asteroidPrefabs;
+    public GameObject sensorPrefab;
     int asteroidIndex;
     public Vector3 spawnPos;
+    Vector3 sensorSpawnPos;
     public float spawnRange;
     public float startDelay;
     public float spawnInterval;
+    public float sensorSpawnInterval;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnRandomAsteroid", startDelay, spawnInterval);
+        InvokeRepeating("SpawnSensor", startDelay, sensorSpawnInterval);
     }
 
     // Update is called once per frame
@@ -28,5 +32,11 @@ public class SpawnManager : MonoBehaviour
         asteroidIndex = Random.Range(0, asteroidPrefabs.Length);
         spawnPos = new Vector3(Random.Range(-spawnRange, spawnRange), -1, 1.6f);
         Instantiate(asteroidPrefabs[asteroidIndex], spawnPos, asteroidPrefabs[asteroidIndex].transform.rotation);
+    }
+
+    void SpawnSensor()
+    {
+        sensorSpawnPos = new Vector3(0, 0, 0);
+        Instantiate(sensorPrefab, sensorSpawnPos, sensorPrefab.transform.rotation);
     }
 }
